ebf910e3d519480901001c391087f2e4c995ed5b
DaikiHIRAYAMA <daiki_hirayama@mediado.jp>
2022-06-21 19:46:06 +0900

    mail text edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
    #before_action :correct_user, only: [:show]
    before_action :authenticate_user!
+   before_action :correct_user_show , only: [:show]
    
 
   def index
@@ -8,9 +9,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
-
-
   end
 
 private
@@ -22,6 +20,11 @@ private
   def correct_user
     @user = User.find(params[:id])
     redirect_to current_user unless current_user?(@user)
+  end
+
+  def correct_user_show
+    @user = User.find(params[:id])
+    redirect_to mypage_path if current_user?(@user)
   end
   
 end
