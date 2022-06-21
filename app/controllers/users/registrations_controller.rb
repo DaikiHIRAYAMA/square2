09377f00589fa802898639aa51f8053023b2cc86
DaikiHIRAYAMA <daiki_hirayama@mediado.jp>
2022-06-21 19:53:03 +0900

    add validation

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -66,7 +66,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
    def correct_user
     @user = User.find(params[:id])
-    redirect_to current_user unless current_user?(@user)
+    redirect_to root_path unless current_user?(@user)
   end
 
 end
