902f382da25fdef9104434e58155051c86d6d61e
DaikiHIRAYAMA <daiki_hirayama@mediado.jp>
2022-06-22 10:31:27 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ #<User id: 2, email: "user2@sample.com", created_at: "2022-06-21 10:15:35", updated_at: "2022-06-21 10:16:10", name: "user2">,
+
+
+5.times do |n|
+    User.create!(
+      email: "test#{n + 1}@test.com",
+      name: "貸し借り太郎#{n + 1}",
+      password: "password",
+      #password_confirmation: "password",
+      confirmed_at: Time.now,
+    )
+  end
