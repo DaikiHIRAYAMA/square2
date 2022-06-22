b0d09047c4fbad0e1d8f7996fe78d10f9b94faca
DaikiHIRAYAMA <daiki_hirayama@mediado.jp>
2022-06-22 13:43:58 +0900

    reset

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def render_500
-    render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'html'
+    render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
   end
     
 
