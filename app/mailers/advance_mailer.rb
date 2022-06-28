87eb4d43eebe95128efe46dc8ee276f62a8a0e24
DaikiHIRAYAMA <daiki_hirayama@mediado.jp>
2022-06-28 12:12:45 +0900

    mail system change

--- app/mailers/advance_mailer.rb
+++ app/mailers/advance_mailer.rb
@@ -1,19 +1,27 @@
 class AdvanceMailer < ApplicationMailer
 
     def send_the_day_before
-      @records = Record.where(current_situation: "borrow").or(Record.where(current_situation: "rend"))
-      @records.each do |record|
-        
-       if record.time_limit.strftime("%Y-%m-%d") == DateTime.tomorrow.strftime("%Y-%m-%d") #期日前日で
+      @records1 = Record.where(current_situation: "borrow").or(Record.where(current_situation: "rend")) #貸している状態か借りている状態のもの
+      @records2 = @records1.where(time_limit: DateTime.tomorrow.strftime("%Y-%m-%d"))
+       #{DateTime.tomorrow}" ).or(@records1.where("time_limit > #{DateTime.yesterday}"))
+       #期日が明日のもの      
+      @records2.each do |record| #それら一つづつに
 
-        @transaction = Transaction.find(record.transaction_id)
+        AdvanceMailer.send_mail(record).deliver_now
 
 
-        mail to: "#{ User.find(@transaction.render_id).email},#{User.find(@transaction.borrower_id).email }"
-        mail subject: "返済期日が迫っております"
+
+      #  mail to: "#{ User.find(@transaction.render_id).email},#{User.find(@transaction.borrower_id).email }"
+      #  mail subject: "返済期日が迫っております"
  
         end
-
-      end
     end
+
+     def send_mail(record)
+
+      @transaction = Transaction.find(record.transaction_id)
+
+      mail to: "#{ User.find(@transaction.render_id).email},#{User.find(@transaction.borrower_id).email }"
+      mail subject: "返済期日が迫っております"
+     end
 end
